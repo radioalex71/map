5b0a2e6a099fad05e7663ef4afcff08f2e0a3a02
Alexey Ryabinkin <a.ryabinkin71@gmail.com>
2025-11-08 22:30:50 +0300

    Lesson_03

---
+++ Lesson_03/Task_01/Task_01.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <future>
+#include <vector>
+#include <random>
+
+void sort(std::vector<int>& v)
+{
+    auto n = v.size();
+    for (int i = 0; i < n - 1; i++)
+
+    {
+        int minElement = v[i];
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((v[j] < minElement) && (minElement != v[j]))
+            {
+                minElement = v[j];
+                std::swap(v[i], v[j]);
+            }
+        }
+    }
+}
+
+void findMin(const std::vector<int> v, int i, int vSize, std::promise<int> p)
+{
+    int minIndex = i;
+    for (int j = i + 1; j < vSize; j++)
+    {
+        if (v[j] < v[minIndex])
+        {
+            minIndex = j;
+        }
+    }
+    p.set_value(minIndex);
+}
+
+void printVector(const std::vector<int>& v)
+{
+    for (const auto& i : v)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> v = { 8, 4, 3, 2, 5, 6, 7, 1, 9, 10 };
+    int vecSize = v.size();
+    std::cout << "Original vector: ";
+    printVector(std::ref(v));
+    for (int i = 0; i < vecSize - 1; i++)
+    {
+        std::promise<int> p;
+        std::future<int> f = p.get_future();
+        auto asyncFindMin = std::async(std::launch::async, findMin, std::ref(v), i, vecSize, std::move(p));
+        int minIndex = f.get();
+        if (v[i] != v[minIndex])
+        {
+            std::swap(v[i], v[minIndex]);
+        }
+    }
+    std::cout << "Sorted vector: ";
+    printVector(std::ref(v));
+
+    return 0;
+}
---
+++ Lesson_03/Task_02/Task_02.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <future>
+#include <vector>
+//#include "Windows.h"
+
+typedef unsigned int ui;
+
+int increment(int value)
+{
+    return value + 1;
+}
+
+template <typename It, typename Func>
+void async_for_each(It first, It last, Func func)
+{
+    const ui length = std::distance(first, last);
+    if (!length)
+        return;
+    const ui min_per_thread = 25;
+    if (length < (2 * min_per_thread))
+    {
+        std::for_each(first, last, func);
+    }
+    else
+    {
+        It const mid_point = first + length / 2;
+        std::future<void> first_half = std::async(&async_for_each<It, Func>, first, mid_point, func);
+        std::future<void> second_half = std::async(&async_for_each<It, Func>, mid_point, last, func);
+        first_half.get();
+        second_half.get();
+    }
+}
+
+void printVector(std::vector<int>& vec)
+{
+    for (const auto& elem : vec)
+        std::cout << elem << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector<int> vec = { 11, 22, 3, 4, 55, 6, 7, 8, 9, 10 };
+    printVector(vec);
+    async_for_each(vec.begin(), vec.end(), [](int& elem) { elem = increment(elem); });
+}
